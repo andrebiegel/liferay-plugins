d629b6f577c58ed9a1cee69acfd0cce991a8d4e2
andrebiegel <andre.biegel@icloud.com>
2026-05-30 21:23:56 +0200

    synchroniers messaging hsa been removed from the message bus

---
+++ message-bus/README.md
@@ -0,0 +1,4 @@
+# Liferay Message Bus
+
+_MessageBusEventListener_ and _DestinationEventListener_ removed since 2023 Q4 see https://learn.liferay.com/w/dxp/self-hosted-installation-and-upgrades/upgrading-liferay/deprecations-and-breaking-changes-reference/7-4-deprecations-and-breaking-changes/7-4-breaking-changes
+
--- message-bus/src/main/java/io/github/andrebiegel/mb/MySink.java
+++ message-bus/src/main/java/io/github/andrebiegel/mb/MySink.java
@@ -1,18 +1,27 @@
 package io.github.andrebiegel.mb;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
-
 import org.osgi.service.component.annotations.Component;
 
-@Component (
-    immediate = true,
-    property = {"destination.name=myDestinationName"},
-    service = MessageListener.class
+@Component(
+		immediate = true,
+		property = { "destination.name=github/message_me" },
+		service = MessageListener.class
 )
-public class MySink implements MessageListener {
-
-   public void receive(Message message) {
-       System.out.println(getClass().getName()+  " destination.name=myDestinationName received ..." +message.get("foo"));
-   }
+public class MySink extends BaseMessageListener {
+	
+	@Override
+	protected void doReceive(Message message) throws Exception {
+		if (_log.isInfoEnabled()) {
+			_log.info(getClass().getName() + " destination.name=github/message_me received ..." + message.get("foo"));
+		}
+	}
+	
+	private static final Log _log = LogFactoryUtil.getLog(
+			MySink.class);
+	
 }
