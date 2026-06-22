fabafd61168fe18baeebe5ba48263d61c1b1961a
andrebiegel <andre.biegel@icloud.com>
2026-06-22 09:43:20 +0200

    adding getter and setter and default constructor for jackson

--- jaxrs-resource/src/main/java/io/github/andrebiegel/entity/Drink.java
+++ jaxrs-resource/src/main/java/io/github/andrebiegel/entity/Drink.java
@@ -1,7 +1,21 @@
 package io.github.andrebiegel.entity;
 public class Drink {
-    public String name; 
+    private String name;
     public Drink (String name){
+        this.name = name;
+    }
+    
+    /**
+     * default contructor for jackson
+     */
+    public Drink(){}
+  
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
         this.name = name;
     }
 }
